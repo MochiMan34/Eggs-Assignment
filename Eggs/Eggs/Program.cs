3ec540fece89955d23a0da66579fb9bf61fe603a
MochiMan34 <st27567@email.vccs.edu>
2017-11-10 16:05:38 -0500

    Add files via upload

---
+++ Eggs/Eggs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eggs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculateEggs(); 
+        }
+
+        public static void calculateEggs()
+        {
+            int numOfEggs1 = 89;
+            int numOfEggs2 = 56;
+            int numOfEggs3 = 83;
+            int numOfEggs4 = 91;
+
+
+            int sumOfEggs = numOfEggs1 + numOfEggs2 + numOfEggs3 + numOfEggs4;
+
+            int dozens = (sumOfEggs / 12); 
+            int leftOvers = (sumOfEggs % 12);
+
+            System.Console.WriteLine("There is a total of: " + sumOfEggs + " Which equates to: " + dozens + " Dozens and " + leftOvers + " Eggs");
+            System.Console.ReadKey(); 
+
+
+        }
+    }
+}
---
+++ EggsInteractiveGUI/EggsInteractiveGUI/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EggsInteractiveGUI
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String[] numOfEggsInput = {textBox1.Text, textBox2.Text , textBox3.Text , textBox4.Text , textBox5.Text };
+            int[] numOfEggs = new int[5];
+            int sumOfEggs = 0; 
+            for(int i = 0; i < numOfEggs.Length; i++)
+            {
+                numOfEggs[i] = Convert.ToInt32(numOfEggsInput[i]);
+                sumOfEggs += numOfEggs[i];
+
+            }
+
+            
+
+           
+           
+          
+
+
+            int dozens = sumOfEggs / 12;
+            int remainders = sumOfEggs % 12;
+            String answer = sumOfEggs + " Eggs , which is: " + dozens + " dozens" + " and " + remainders + " eggs";
+            
+
+            label2.Text = "Total of: " + answer; 
+            
+
+          
+
+
+
+        }
+    }
+}
